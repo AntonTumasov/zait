162fd74d005379d4000a3c4e47e2035823c20371
aboyko <aboyko@herzen.spb.ru>
2016-05-19 01:02:30 +0300

    Fixed bug with require. Wrapped by closure.

--- src/modules/config.js
+++ src/modules/config.js
@@ -1,42 +1,49 @@
 const fs = require('fs'),
-  parser = require('parser.js');
+  parser = require('./parser.js');
 
-exports.config = {
-  _confParser: (function () {
-    const confParser = casper.cli.get('parser') || 'json';
-    
-    return confParser
-      .toLowerCase()
-      .trim();
-  })(),
-  _file: undefined,
-  get raw() {
-    if (!this._file) {
-      let configPath = casper.cli.get('config') || 'pageload.json' ||
+/**
+ * @namespace Configuration handling
+ * 
+ * @param {Object} CasperJS instance
+ */
+exports.config = function (casper) {
+  return {
+    _confParser: (function () {
+      const confParser = casper.cli.get('parser') || 'json';
+
+      return confParser
+        .toLowerCase()
+        .trim();
+    })(),
+    _file: undefined,
+    get raw() {
+      if (!this._file) {
+        let configPath = casper.cli.get('file') || 'pageload.json' ||
           'pageload.yml',
-        conf = fs.read(configPath.trim());
-      
-      this._file = conf; //narrow place(memory)
+          conf = fs.read(configPath.trim());
 
-      return conf;
-    } else {
-      return this._file;
-    }
-  },
-  get parsed() {
-    switch (this._confParser) {
+        this._file = conf; //narrow place(memory)
+
+        return conf;
+      } else {
+        return this._file;
+      }
+    },
+    get parsed() {
+      switch (this._confParser) {
       case 'json':
         return JSON.parse(this.raw);
       case 'yml':
         return;
       default:
         throw new Error('There is no parser for this file.'); //@TODO make custom errors
+      }
+    },
+    get commands() {
+      return this.parsed.commands;
+    },
+    get reporter() {
+      return this.parsed.reporter;
     }
-  },
-  get commands() {
-    return this.parsed.commands;
-  },
-  get reporter() {
-    return this.parsed.reporter;
   }
 };
