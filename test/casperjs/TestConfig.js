1dc969880a7538a41ee86aacf6349c952ef6d51c
aboyko <aboyko@herzen.spb.ru>
2016-05-26 01:25:24 +0300

    Fixed bugs

--- test/casperjs/TestConfig.js
+++ test/casperjs/TestConfig.js
@@ -1,30 +1,33 @@
+casper.on('error', function (err) {
+      this.log(err, 'error');
+      this.exit(1);
+});
+
 const configModule = require('../src/modules/config.js'),
   assert = require('chai').assert,
   sinon = require('sinon');
 
 describe('Configuration handler', function () {
-  let casper = {},
-      config;
+
+  let cliArgumentsStorage,
+    config;
   
   before(function () {
-    casper = {
-      cli: {
-      },
-      _cliArgumentsStorage: {
+    cliArgumentsStorage = {
         parser: undefined,
-        file: undefined
-      }
+        file: './build/test/pageload.json' 
     };
-    
+
     casper.cli.get = sinon.stub();
     
     casper.cli.get
       .withArgs('parser')
-      .returns(casper._cliArgumentsStorage.parser)
+      .returns(cliArgumentsStorage.parser)
       .withArgs('file')
-      .returns(casper._cliArgumentsStorage.file);
+      .returns(cliArgumentsStorage.file);
     
     config = configModule.config(casper);
+    casper.start();
   });
   
   //@TODO add dynamic file creation and moving
@@ -32,9 +35,7 @@ describe('Configuration handler', function () {
     it('should return raw file', function () {
       const confFile = config.raw;
       
-      console.log(config)
-      
-      assert.isString(conf);
+      assert.isString(confFile);
     });
   });
 });
